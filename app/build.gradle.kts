95977a33e3e9b1b60187d41ea17609d25a5c9fdb
Ali Bennouna Louridi <alibennouna003@gmail.com>
2023-11-26 16:24:46 +0100

    Design Login Texts et Bouton
    J'ai ajouté des zones d'écritures pour entrer son "username" et son "password", ainsi qu'un bouton pour valider. Aucun test n'est réalisé il s'agit purement d'un test sans fonc particulier.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mobileproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mobileproject"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
