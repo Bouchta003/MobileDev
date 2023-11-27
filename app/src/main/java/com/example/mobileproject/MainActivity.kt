dcca460855354a7a7ab742915b055d53ee0df681
Ali Bennouna Louridi <alibennouna003@gmail.com>
2023-11-27 12:32:06 +0100

    Bouton Fonctionnel et Password caché
    Suite aux recommandations de notre très chère Mélanie H. j'ai fait en sorte que le password soit caché quand on l'entre. Aussi en cas d'erreur de login les entrées sont supprimées pour insinuer que la combinaison ne marche pas, et enfin une réponse Toast (mini pop-up) affiche une réaction en fonction de la combinaison de messages.

--- app/src/main/java/com/example/mobileproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileproject/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.mobileproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //J'inities les variables concernées par l'activité, on utilise VAR et non pas VAL pour qu'elles puissent être modifiées
+        var username = findViewById<EditText>(R.id.username)
+        var password = findViewById<EditText>(R.id.password)
+        var loginButton = findViewById<Button>(R.id.buttonlogin)
+        //Ces values correspondent aux username/password à tester et vérifier
+        val usertest = "testusername"
+        val passwordtest = "testpassword"
+        //Réaction du couton en fonction des inputs username et password:
+        loginButton.setOnClickListener {
+            val usertxt = username.text.toString()
+            val passtxt = password.text.toString()
+            if(usertxt!=usertest&&passtxt!=passwordtest){
+                username.text=null
+                password.text=null
+                Toast.makeText(this@MainActivity, "Invalid username or, please try again", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this@MainActivity, "Login successful, welcome !!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
